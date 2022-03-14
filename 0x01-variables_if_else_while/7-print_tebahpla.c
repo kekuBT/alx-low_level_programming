81dc4bafcc3e0fa43ad9707c4a0e44c3ba5e0012
kekubt <kekubt@gmail.com>
2022-03-14 19:10:56 +0300

    my commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 1 (Success/correct)
+*/
+int main(void)
+
+{
+	char alphabet;
+
+	for (alphabet = 122; alphabet >= 97; alphabet++)
+
+	{
+	putchar(alphabet);
+	}
+
+	putchar(\n);
+
+	return(0);
+}
