237df03c013950161a0f37e86343b34c4f11a274
kekubt <kekubt@gmail.com>
2022-03-14 18:29:29 +0300

    my commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,20 +7,22 @@
 int main(void)
 
 {
-	char lower_case;
-	char upper_case;
+        char lower_case;
+        char upper_case;
 
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-	{
-	putchar(upper_case);
-	}
+        for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+        {
+        putchar(lower_case);
+        }
 
-	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-	{
-	putchar(lower_case);
-	}
+        for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+        {
+        putchar(upper_case);
+        }
 
-	putchar('\n');
+        
 
-	return (0);
+        putchar('\n');
+
+        return (0);
 }
