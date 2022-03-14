6160fd284c54429d0ab10e61e0aeac5592421a56
kekubt <kekubt@gmail.com>
2022-03-14 18:56:52 +0300

    my commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,14 +10,14 @@ int main(void)
 
 	int digit;
 
-	for (digit = 0; digit < 10; digit++)
-	{
+	for (digit = 0 ; digit < 10; digit++)
 
+	{
 	printf("%digit", digit);
 	}
 
 	printf("\n");
 
-	return(0);
+	return (0);
 
 }
