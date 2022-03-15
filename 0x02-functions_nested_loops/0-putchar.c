5ff27b00998b47e5c7858dcb198b29f679173b40
kekubt <kekubt@gmail.com>
2022-03-16 01:35:15 +0300

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,16 @@
  **/
 int main(void)
 {
-        char ermias[] = "_putchar";
+	char ermias[] = "_putchar";
 
-        int c;
+	int c;
 
-        for (c = 0; c < 8; c++)
-        {
-                _putchar(ermias[c]);
-        }
-        _putchar('\n');
-        return (0);
+	for (c = 0; c < 8; c++)
+
+	{
+		 _putchar(ermias[c]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
