9067ea7ea2f7a8e0238e1fea175dfacd57c010a8
kekubt <kekubt@gmail.com>
2022-03-16 00:26:08 +0300

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 1 (Success/correct)
+*/
+
+int main()
+{
+    printf("_putchar\n");
+    return 0;
+}
