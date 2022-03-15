f897199d7aede072aec98b3ba06ddf10c0b302ba
kekubt <kekubt@gmail.com>
2022-03-16 02:13:39 +0300

    my commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_alphabet_10x - prints the alphabet in lowercase 10 times
+* print_alphabet_x10 - prints the alphabet in lowercase 10 times
 *
 * Return: Always 0.
 **/
