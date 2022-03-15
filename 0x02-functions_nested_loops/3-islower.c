f138ec90d5a3bc07d87bb4bfe231ede1e9cfd001
kekubt <kekubt@gmail.com>
2022-03-16 02:12:44 +0300

    my commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* main - entry block
-* @void: no argument
-* Return: 0
+* _islower - returns 1 if lowercase alphabet and 0 otherwise
+* @c: takes in a character
+* Return: 0 for uppercase, 1 for lowercase
 **/
 int _islower(int c)
 {
