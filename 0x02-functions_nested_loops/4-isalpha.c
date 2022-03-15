47e4a2ea2a8f3023907a351c3717b619a394a0df
kekubt <kekubt@gmail.com>
2022-03-16 02:00:38 +0300

    my commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabet in lowercase, followed by a new line
+ * You can only use the putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	if ((c >= 'A' && c  <= 'z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
