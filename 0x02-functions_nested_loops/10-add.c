66719a19d7a3baccaafc5233c6b2d16c120d1704
kekubt <kekubt@gmail.com>
2022-03-16 11:02:05 +0300

    my commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -9,11 +9,14 @@ void print_times_table(int n)
 
 	if (n > 15 || n < 0)
 		return;
+
 	for (i = 0; i <= n; i++)
 	{
+
 		for (j = 0; j <= n; j++)
 		{
 			k = i * j;
+
 			if (j == 0)
 			{
 				_putchar(k + '0');
