415ffec01146995942856cd5a96b5a20e0737911
kekubt <kekubt@gmail.com>
2022-03-16 11:05:21 +0300

    my commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,51 +1,11 @@
 #include "main.h"
 /**
- * print_times_table - prints the times table
- * @n: takes in an integer
+ * add - function that adds two integers together
+ * @a: takes in an integer
+ * @b: takes in an integer
+ * Return: a + b result
  */
-void print_times_table(int n)
+int add(int a, int b)
 {
-	int i, j, k;
-
-	if (n > 15 || n < 0)
-		return;
-
-	for (i = 0; i <= n; i++)
-	{
-
-		for (j = 0; j <= n; j++)
-		{
-			k = i * j;
-
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
-			else if (k <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			}
-			else if (k >= 10 && k < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 100) + '0');
-				_putchar(k / 10 % 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-		}
-		_putchar('\n');
-	}
+	return (a + b);
 }
