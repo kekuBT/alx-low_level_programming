848e2ebc3cfe9be950f85746674bdf1aeab28a12
kekuBT <69989635+kekuBT@users.noreply.github.com>
2022-03-24 22:39:10 +0300

    2nd question

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,6 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n && *(dest + i) != '\0')
 	while (i < n && *(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
@@ -21,5 +20,6 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	while (i != n)
 		dest[i++] = '\0';
+
 	return (dest);
 }
