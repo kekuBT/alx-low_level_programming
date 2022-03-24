edfb6359a65f61b256ad0e1ef06e70d92f159874
kekuBT <69989635+kekuBT@users.noreply.github.com>
2022-03-24 20:17:56 +0300

    0 question

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,24 @@
 
 char *_strcat(char *dest, char *src)
 {
-	unsigned int i = 0;
-	unsigned int j = 0;
+	int i, j;
 
-	while (*(dest + i) != '\0')
-		i++;
-	while (*(src + j) != '\0')
+	i = 0;
+
+	while (dest[i] !\0)
 	{
-		*(dest + i) = *(src + j);
+		i++;
+	}
+
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
+	dest[i] = src[j];
+
 	return (dest);
 }
