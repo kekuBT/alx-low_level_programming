f7513b0345610ede4bbce0f0fe1d5a70f1ad4fc7
kekuBT <69989635+kekuBT@users.noreply.github.com>
2022-04-08 02:31:05 +0300

    1st question

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-
 /**
- * main - Prints the program name, followed by a new line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always o.
- */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+  *main - prints the  name of this program.
+  *@argc: counts number of arguemnts paased in th command line.
+  *@argv: stores arguments passed in the command line.
+  *
+  *Return: 0 on success.
+  */
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	(void)argc;
 
+	printf("%s\n", argv[0]);
 	return (0);
 }
